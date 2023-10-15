7657dcbe0fcd5fbd2fc299653dda30fface94220
PedroCalaisf <pedrocalaisf@gmail.com>
2023-10-15 14:48:34 -0300

    Commit Lista Java For

---
+++ ListaJavaAt12.java
@@ -0,0 +1,26 @@
+package lista.java;
+
+import java.util.Scanner;
+
+public class ListaJavaAt12 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número para a tabuada de multiplicação: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Tabuada de multiplicação para " + numero + ":");
+
+        for (int i = 1; i <= 10; i++) {
+            int resultado = numero * i;
+            System.out.println(numero + " x " + i + " = " + resultado);
+        }
+
+        scanner.close();
+    }
+
+
+	}
+
+
